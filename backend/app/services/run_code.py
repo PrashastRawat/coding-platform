8642538bf9c7ceb043cafa442624bcf6d25253fc
PrashastRawat <your@email.com>
2026-03-07 14:22:28 +0530

    fix: use python3 command on Render

--- backend/app/services/run_code.py
+++ backend/app/services/run_code.py
@@ -41,7 +41,7 @@ def run_code(code: str, input_data: str):
         start = time.time()
 
         result = subprocess.run(
-            ["python", file_name],
+            ["python3", file_name],
             input=input_data,
             text=True,
             capture_output=True,
